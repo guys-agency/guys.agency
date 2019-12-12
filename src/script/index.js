e4b7da623ffa0551e7b6bcbbb7474abd03d9f074
Graev <graev.k@gmail.com>
2019-12-12 04:09:58 +0300

    /мат/
    Это все рефакторить надо, кажется оч много сделано сначало киданием палки в колесо, а потом ремонт самого колеса... но чет не мог себя отпустить спать, пока не работало... Добавил кидание сообщений от одного автора в message__text-wrapper

--- src/script/index.js
+++ src/script/index.js
@@ -1,35 +1,4 @@
 import * as rick from './rick.js';
 import WorkWithData from './renderMessages.js';
 
-const dataTest = [
-	{
-		name: 'Данил Леонов',
-		text:
-			'Ребят, привет! Нужно решить по сайту-портфолио для нас.<br>Разделы: <a href="#about">о нас</a>, <a href="#works">услуги/работы</a>, <a href="#contact">контакты</a>',
-		color: '#FFFFFF',
-		photo: './images/Message/AvatarLeo.png',
-	},
-	{
-		name: 'Константин Граев',
-		text: [
-			'Слушай, ну, нужно что-то инфостильное в духе:',
-			'Йоу, мы команда друзей из <a href="">дизайнеров</a>, <a href="">разработчика</a> и <a href="">маркетолога</a>.<br>Наша специальность — брендинг и веб-интерфейсы. Возьмем самое сложное и сделаем это простым.',
-		],
-		color: '#27AE60',
-		photo: './images/Message/AvatarKos.png',
-	},
-	{
-		name: 'Ана Водолазко',
-		text: 'Всем привет, я уже подготовила материал',
-		color: '#2F80ED',
-		photo: './images/Message/AvatarAnya.png',
-	},
-	{
-		name: 'Георгий Каташев',
-		text: 'Окей, бумер...',
-		color: '#EAF2FD',
-		photo: './images/Message/AvatarG.png',
-	},
-];
-
-const workWithData = new WorkWithData(dataTest);
+const workWithData = new WorkWithData();
--- src/script/renderMessages.js
+++ src/script/renderMessages.js
@@ -3,13 +3,8 @@ import { api } from './api.js';
 const blocks = ['start', 'about', 'works', 'contacts'];
 
 export default class WorkWithData {
-	constructor(data) {
-		this.data = data;
-		// data.forEach(litlData => {
-		// 	console.log('data', litlData);
-		// 	this.createElem('start', litlData);
-		// });
-		console.log('object', api.getMessageByBlock('start'));
+	constructor() {
+		// console.log('object', api.getMessageByBlock('start'));
 		this.createElem = this.createElem.bind(this);
 		blocks.forEach(block => {
 			this.renderBlock(block);
@@ -18,94 +13,111 @@ export default class WorkWithData {
 
 	renderBlock(block) {
 		api.getMessageByBlock(block).then(litlData => {
-			console.log('data', litlData);
+			// console.log('data', litlData);
 			const newObj = { data: {} };
 			Object.keys(litlData.data)
 				.sort((a, b) => {
 					return litlData.data[a].index - litlData.data[b].index;
 				})
-				.forEach(i => {
+				.reduce((prval, i, index, arr) => {
+					console.log('arr', arr);
+					if (i < arr.length - 1) {
+						console.log('i', i);
+						if (
+							litlData.data[Number(i)].owner._id === prval ||
+							litlData.data[Number(i)].owner._id ===
+								litlData.data[Number(i) + 1].owner._id
+						) {
+							console.log(
+								prval,
+								litlData.data[Number(i)].owner._id,
+								litlData.data[Number(i) + 1].owner._id
+							);
+							this.createElem(block, litlData.data[i], true);
+							return litlData.data[Number(i)].owner._id;
+						}
+					}
 					newObj.data[i] = litlData.data[i];
-					this.createElem(block, litlData.data[i]);
-				});
-			console.log('objectnewObj', newObj);
+					this.createElem(block, litlData.data[i], false);
+					return litlData.data[Number(i)].owner._id;
+				}, 0);
+			// console.log('objectnewObj', newObj);
 		});
 	}
 
-	createElem(block, litlData) {
+	createElem(block, litlData, nMessage) {
 		const messageContainer = document.createElement('div');
 		const img = document.createElement('img');
+		const text = document.createElement('div');
+		const textContainer = document.createElement('div');
+
+		messageContainer.classList.add('message__container');
+
+		text.innerHTML = litlData.text;
+		textContainer.classList.add('message__text-container');
+		text.classList.add('message__text');
+
 		img.src = litlData.owner.avatar;
 		img.classList.add('message__pic');
-		messageContainer.classList.add('message__container');
-		messageContainer.appendChild(img);
 
-		if (Array.isArray(litlData.text)) {
+		if (nMessage) {
+			console.log(
+				'jq',
+				$('.message__container')
+					.last()
+					.find('.message__text-wrapper')
+			);
+
 			const textWrapper = document.createElement('div');
 			textWrapper.classList.add('message__text-wrapper');
-			for (let i = 0; i < litlData.text.length; i++) {
-				const textContainer = document.createElement('div');
-				const text = document.createElement('div');
 
-				text.innerHTML = litlData.text[i];
-				textContainer.classList.add('message__text-container');
-				text.classList.add('message__text');
-				textContainer.appendChild(text);
-				textContainer.style = `background-color: ${litlData.color};`;
+			textContainer.appendChild(text);
+			this.chooseClass(messageContainer, litlData.owner._id);
+			textWrapper.appendChild(textContainer);
 
-				if (litlData.color === '#FFFFFF') {
-					// text.style = `color: #000`;
-					// messageContainer.style = `align-self: flex-end; flex-direction: row-reverse;`;
-					// textContainer.style = `border: solid 1px #BDBDBD;`;
-					// img.style = `margin-left: 15px; margin-right: 0px;`;
-					messageContainer.classList.add('message__container_d');
-				}
-				if (litlData.color === '#27AE60') {
-					messageContainer.classList.add('message__container_k');
-				}
-				if (litlData.color === '#2F80ED') {
-					messageContainer.classList.add('message__container_a');
-				}
-				if (litlData.color === '#EAF2FD') {
-					messageContainer.classList.add('message__container_g');
-				}
-
-				textWrapper.appendChild(textContainer);
+			if (
+				$('.message__container')
+					.last()
+					.find('.message__text-wrapper').length
+			) {
+				console.log('test');
+				messageContainer.appendChild(textWrapper);
+				$('.message__text-wrapper')
+					.last()
+					.append(messageContainer);
+				return;
 			}
+			messageContainer.appendChild(img);
 			messageContainer.appendChild(textWrapper);
 			$(`#${block}`).append(messageContainer);
 		} else {
-			const textContainer = document.createElement('div');
-			const text = document.createElement('div');
 			text.innerHTML = litlData.text;
-			img.src = litlData.owner.avatar;
-			messageContainer.classList.add('message__container');
-			textContainer.classList.add('message__text-container');
-			text.classList.add('message__text');
+
+			// img.src = litlData.owner.avatar;
+			// messageContainer.classList.add('message__container');
 			img.classList.add('message__pic');
 			textContainer.appendChild(text);
 			messageContainer.appendChild(img);
 			messageContainer.appendChild(textContainer);
-			// textContainer.style = `background-color: ${litlData.color}`;
 
-			if (litlData.owner._id === '5ded05e46180969dc7a73838') {
-				// text.style = `color: #000`;
-				messageContainer.style = `align-self: flex-end; flex-direction: row-reverse;`;
-				messageContainer.classList.add('message__container_d');
-				// textContainer.style = `border: solid 1px #BDBDBD; background-color: ${litlData.color};`;
-				// img.style = `margin-left: 15px; margin-right: 0px;`;
-			}
-			if (litlData.owner._id === '5ded0461efaf9a9dc7729105') {
-				messageContainer.classList.add('message__container_k');
-			}
-			if (litlData.owner._id === '5ded06626180969dc7a7383a') {
-				messageContainer.classList.add('message__container_a');
-			}
-			if (litlData.owner._id === '5ded063a6180969dc7a73839') {
-				messageContainer.classList.add('message__container_g');
-			}
+			this.chooseClass(messageContainer, litlData.owner._id);
 
 			$(`#${block}`).append(messageContainer);
+		}
+	}
+
+	chooseClass(v, id) {
+		if (id === '5ded05e46180969dc7a73838') {
+			v.classList.add('message__container_d');
+		}
+		if (id === '5ded0461efaf9a9dc7729105') {
+			v.classList.add('message__container_k');
+		}
+		if (id === '5ded06626180969dc7a7383a') {
+			v.classList.add('message__container_a');
+		}
+		if (id === '5ded063a6180969dc7a73839') {
+			v.classList.add('message__container_g');
 		}
 	}
 }
